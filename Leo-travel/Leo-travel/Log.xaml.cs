6a0e8cff6b01ff5e74272ffa9ccfe05c1ff5ca58
Nastya Sheptur <nasya2611@gmail.com>
2015-04-08 17:13:09 +0300

    3 commit

---
+++ Leo-travel/Leo-travel/Log.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace Leo_travel
+{
+    /// <summary>
+    /// Логика взаимодействия для Log.xaml
+    /// </summary>
+    public partial class Log : Window
+    {
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string myConnection = "datasource=localhost;port=3306;username=root;password=1101;";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand("select * database.edata", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                myConn.Open();
+                leoDataSet ds = new leoDataSet();
+                MessageBox.Show("Connected");
+                myConn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+    }
+}
