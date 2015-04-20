07954e40698739ba3ce9d09f74bcc547eed0d4d8
Nastya Sheptur <nasya2611@gmail.com>
2015-04-20 18:12:14 +0300

    4 commit
    20.04.15 зміни

--- Leo-travel/Leo-travel/MainWindow.xaml.cs
+++ Leo-travel/Leo-travel/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace Leo_travel
 {
@@ -20,16 +22,51 @@ namespace Leo_travel
     /// </summary>
     public partial class MainWindow : Window
     {
+       
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            
+
             Log w1 = new Log();
             w1.Show();
         }
+
+        private void Load_table_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string myConnection = "datasource=localhost;port=3306;username=root;password=1101;";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand("select * database.edata", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAdapter);
+                myConn.Open();
+                string Query = "Select * user ";
+                MySqlCommand createCommand = new MySqlCommand(Query, myConn);
+                createCommand.ExecuteNonQuery();
+
+                myConn.Close();
+               
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
+        
+        
+       
+      
+        
+       
     }
-}
+  
+    }
