9076bd5a5ff2183536cab5a94cbf4f715ae747b3
Nastya Sheptur <nasya2611@gmail.com>
2015-05-11 03:40:10 +0300

    11.05 Nigth
    + загрузка

--- Leo-travel/Leo-travel/MainWindow.xaml.cs
+++ Leo-travel/Leo-travel/MainWindow.xaml.cs
@@ -14,15 +14,41 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using MySql.Data.MySqlClient;
+using System.IO;
+using System.Windows.Threading;
+
 
 namespace Leo_travel
 {
+    
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        private int time = 20;
+        load l = new load();
+        
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+            l.Show();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (time > 10)
+            {
+                time--;
+                timer.Stop();
+                l.Close();
+            }
+        }
+ 
         public MainWindow()
         {
             InitializeComponent();
@@ -83,8 +109,77 @@ namespace Leo_travel
             public Uri ImageFilePath { get; set; }
             public Uri InfoFilePath { get; set; }
         }
+
+        private void DataGrid2_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                
+                string myConnection = "datasource=localhost;port=3306;username=root;password=1101;Database=leo;";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAd = new MySqlDataAdapter("Select * FROM images", myConn);
+                MySqlCommandBuilder cb = new MySqlCommandBuilder(myDataAd);
+                myConn.Open();
+                
+                string Query = "Select * FROM images ";
+                
+                DataTable dt = new DataTable("images");
+                myDataAd.Fill(dt);
+                DataGrid2.ItemsSource = dt.DefaultView;
+                myDataAd.Update(dt);
+                MySqlCommand createCommand = new MySqlCommand(Query, myConn);
+                createCommand.ExecuteNonQuery();
+
+                myConn.Close();
+
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+
         
 
+        //public class IntToImageConverter : IValueConverter
+        //{
+
+        //    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        //    {
+        //        ImageSource result = null;
+        //        var intValue = (int)value;
+        //        switch (intValue)
+        //        {
+        //            case 0:
+        //                {
+        //                    result = new BitmapImage(new Uri(@"your_path_to_image_0"));
+        //                    break;
+        //                }
+
+        //            case 1:
+        //                {
+        //                    result = new BitmapImage(new Uri(@"your_path_to_image_1"));
+        //                    break;
+        //                }
+        //        }
+        //        return result;
+        //    }
+
+        //    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        //    {
+        //        throw new NotImplementedException();
+        //    }
+        //}
+
+        
 
 
 
---
+++ Leo-travel/Leo-travel/load.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Interop;
+using System.Windows.Threading;
+
+namespace Leo_travel
+{
+    /// <summary>
+    /// Логика взаимодействия для load.xaml
+    /// </summary>
+    public partial class load : Window
+    {
+
+        public load()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(MainWindow_Loaded);
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(@"E:\Projects\Leo-travel\Leo-travel\Leo-travel\pics\waiting.gif", UriKind.RelativeOrAbsolute);
+            bi.EndInit();
+            imBackGround.Source = bi;
+            _source = GetSource();
+            imBackGround.Source = _source;
+            ImageAnimator.Animate(_bitmap, OnFrameChanged);
+        }
+        Bitmap _bitmap;
+
+        private BitmapSource GetSource()
+        {
+            if (_bitmap == null)
+            {
+                _bitmap = new Bitmap(@"E:\Projects\Leo-travel\Leo-travel\Leo-travel\pics\waiting.gif");
+            }
+            IntPtr handle = IntPtr.Zero;
+            handle = _bitmap.GetHbitmap();
+            return Imaging.CreateBitmapSourceFromHBitmap(
+                    handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+        }
+        BitmapSource _source;
+
+        private void FrameUpdatedCallback()
+        {
+            ImageAnimator.UpdateFrames();
+            if (_source != null)
+                _source.Freeze();
+            _source = GetSource();
+            imBackGround.Source = _source;
+            InvalidateVisual();
+        }
+
+        private void OnFrameChanged(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                                    new Action(FrameUpdatedCallback));
+        }
+    }
+
+}
